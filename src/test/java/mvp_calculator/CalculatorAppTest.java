49e7bf95076a89dfdc01f0e52c53aedfaee6d7a4
NayabMaryam <nayabmaryam40@gmail.com>
2025-11-01 14:24:07 +0500

    Added the CalculatorAppTest and name change of other class

---
+++ src/test/java/mvp_calculator/CalculatorAppTest.java
@@ -0,0 +1,14 @@
+package mvp_calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorAppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
